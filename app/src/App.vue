c2822463c14e763ac9ea1bfa779ee8855d07bf99
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-05 23:06:54 +0800

    toggling the VNavigationDrawer using VAppBar

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,21 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const isDrawerOpen : Ref<boolean> = ref(false) 
+</script>
+
+
 <template>
-  <v-app>
+  <VApp>
+    <VAppBar :elevation="2" color="primary" scroll-behaviour="elevate" app>
+      <VAppBarNavIcon @click.stop="isDrawerOpen = !isDrawerOpen"></VAppBarNavIcon>
+      <VAppBarTitle> Assignment Home </VAppBarTitle>
+    </VAppBar>
+    <VNavigationDrawer v-model="isDrawerOpen" left app>
+      <VListItem title="My Assignment" subtitle="Done using Vue & Vuetify 3"></VListItem>
+    </VNavigationDrawer>
     <v-main>
       <router-view />
     </v-main>
-  </v-app>
+  </VApp>
 </template>
-
-<script lang="ts" setup>
-  //
-</script>
