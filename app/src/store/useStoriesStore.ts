c0fcd30b136f5cc18fad06a79ad17b0551251a80
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-01-06 10:19:04 +0800

    update interface and data added from API

--- app/src/store/useStoriesStore.ts
+++ app/src/store/useStoriesStore.ts
@@ -13,32 +13,40 @@ export const useStoriesStore = defineStore("stories", {
       const response = await axios.get<IStories>("/");
 
       response.data.data.forEach((story) => {
-        const { title, dek, hero_image_url } = story
-        this.stories.push(story);
-      })
+        const { title, dek, hero_image } = story;
+        const dataToAdd = {
+          title,
+          dek,
+          hero_image_url: hero_image.url,
+        };
+        this.stories.push(dataToAdd);
+      });
     },
   },
 });
 
 interface IStories {
-  /*
-    data and links store Array<IStory> as
-    we're only concerned with these fields for now
-    And using for Intellisense
-  */
   current_page: Number;
-  data: Array<IStory>;
+  data: Array<IStoryData>;
   first_page_url: String;
   from: Number;
   last_page: Number;
   last_page_url: String;
-  links: Array<IStory>;
+  links: Array<IStoryData>;
   next_page_url: String;
   path: String;
   prev_page: Number;
   prev_page_url?: String;
   to: Number;
   total: Number;
+}
+
+interface IStoryData {
+  title: String;
+  dek: String;
+  hero_image: {
+    url: String;
+  };
 }
 
 interface IStory {
